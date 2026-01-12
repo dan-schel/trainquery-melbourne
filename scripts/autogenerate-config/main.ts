c591ba9a22428370faebe7e917e961cc795d1e7f
dan-schel <me@danschellekens.com>
2026-01-12 17:06:12 +1100

    read stops

--- scripts/autogenerate-config/main.ts
+++ scripts/autogenerate-config/main.ts
@@ -1,9 +1,18 @@
 import { env } from "./env";
+import { withGtfsData } from "./utils/with-gtfs-data";
+import { readStops } from "./read-stops";
 
 async function main() {
   const checkMode = process.argv.includes("--check");
 
-  console.log(env.RELAY_KEY);
+  await withGtfsData(env.RELAY_KEY, async ({ regionalDir, suburbanDir }) => {
+    const stops = await readStops(regionalDir, suburbanDir);
+
+    console.log(stops);
+  });
 }
 
-main();
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
---
+++ scripts/autogenerate-config/utils/read-csv.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+import csvParser from "csv-parser";
+import z from "zod";
+
+export async function readCsv<T extends z.ZodType>(
+  path: string,
+  schema: T,
+): Promise<z.infer<T>[]> {
+  return await new Promise((resolve) => {
+    const results: z.infer<T>[] = [];
+    fs.createReadStream(path)
+      .pipe(
+        csvParser({
+          mapHeaders: ({ header }) => header.trim(),
+        }),
+      )
+      .on("data", (row) => {
+        results.push(schema.parse(row));
+      })
+      .on("end", () => {
+        resolve(results);
+      });
+  });
+}
