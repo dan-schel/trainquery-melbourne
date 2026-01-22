8f65c67732340ce2340c2ae9decb272c5d759959
dan-schel <me@danschellekens.com>
2026-01-22 13:32:48 +1100

    add to todo

--- scripts/autogenerate-config/patches/parsed-lines/merge-regional-lines.ts
+++ scripts/autogenerate-config/patches/parsed-lines/merge-regional-lines.ts
@@ -5,3 +5,7 @@
 // This should have the side effect of fixing the bendigo line not realising
 // patterns which stop at Watergardens and not Sunbury and vice-versa can be
 // merged.
+//
+// I imagine this will make use of the consolidateStoppingPatterns and
+// namePatterns functions of find-routes-for-line.ts. They'll need to be
+// exported & maybe moved to their own files.
