484705d9cb7cf9ab61e35c1097988acea2c70550
dan-schel <me@danschellekens.com>
2026-01-23 15:52:58 +1100

    line gtfs ids

--- src/lines/line-gtfs-ids.ts
+++ src/lines/line-gtfs-ids.ts
@@ -1,112 +1,114 @@
 import type { LineGtfsIdMapping } from "../third-party-id-mapping-types.js";
 import * as line from "./line-ids.js";
 
-// prettier-ignore
 export const lineGtfsIds: LineGtfsIdMapping = {
-  // Sandringham
-  "aus:vic:vic-02-SHM:": { lineId: line.SANDRINGHAM },
-  "aus:vic:vic-02-SHM-R:": { lineId: line.SANDRINGHAM, replacementBus: true }, // Replacement bus
+  [line.SANDRINGHAM]: {
+    primary: "aus:vic:vic-02-SHM:",
+    replacementBus: ["aus:vic:vic-02-SHM-R:"],
+  },
 
-  // Frankston
-  "aus:vic:vic-02-FKN:": { lineId: line.FRANKSTON },
-  "aus:vic:vic-02-FKN-R:": { lineId: line.FRANKSTON, replacementBus: true }, // Replacement bus
+  [line.FRANKSTON]: {
+    primary: "aus:vic:vic-02-FKN:",
+    replacementBus: ["aus:vic:vic-02-FKN-R:"],
+  },
 
-  // Stony Point
-  "aus:vic:vic-02-STY:": { lineId: line.STONY_POINT },
-  "aus:vic:vic-02-STY-R:": { lineId: line.STONY_POINT, replacementBus: true }, // Replacement bus
+  [line.STONY_POINT]: {
+    primary: "aus:vic:vic-02-STY:",
+    replacementBus: ["aus:vic:vic-02-STY-R:"],
+  },
 
-  // Cranbourne
-  "aus:vic:vic-02-CBE:": { lineId: line.CRANBOURNE },
-  "aus:vic:vic-02-CBE-R:": { lineId: line.CRANBOURNE, replacementBus: true }, // Replacement bus
+  [line.CRANBOURNE]: {
+    primary: "aus:vic:vic-02-CBE:",
+    replacementBus: ["aus:vic:vic-02-CBE-R:"],
+  },
 
-  // Pakenham
-  "aus:vic:vic-02-PKM:": { lineId: line.PAKENHAM },
-  "aus:vic:vic-02-PKM-R:": { lineId: line.PAKENHAM, replacementBus: true }, // Replacement bus
+  [line.PAKENHAM]: {
+    primary: "aus:vic:vic-02-PKM:",
+    replacementBus: ["aus:vic:vic-02-PKM-R:"],
+  },
 
-  // Gippsland
-  "aus:vic:vic-01-TRN:": { lineId: line.GIPPSLAND },
+  [line.GIPPSLAND]: {
+    primary: "aus:vic:vic-01-TRN:",
+    other: ["aus:vic:vic-01-BDE:"],
+  },
 
-  // Glen Waverley
-  "aus:vic:vic-02-GWY:": { lineId: line.GLEN_WAVERLEY },
-  "aus:vic:vic-02-GWY-R:": { lineId: line.GLEN_WAVERLEY, replacementBus: true }, // Replacement bus
+  [line.GLEN_WAVERLEY]: {
+    primary: "aus:vic:vic-02-GWY:",
+    replacementBus: ["aus:vic:vic-02-GWY-R:"],
+  },
 
-  // Alamein
-  "aus:vic:vic-02-ALM:": { lineId: line.ALAMEIN },
-  "aus:vic:vic-02-ALM-R:": { lineId: line.ALAMEIN, replacementBus: true }, // Replacement bus
+  [line.ALAMEIN]: {
+    primary: "aus:vic:vic-02-ALM:",
+    replacementBus: ["aus:vic:vic-02-ALM-R:"],
+  },
 
-  // Belgrave
-  "aus:vic:vic-02-BEG:": { lineId: line.BELGRAVE },
-  "aus:vic:vic-02-BEG-R:": { lineId: line.BELGRAVE, replacementBus: true }, // Replacement bus
+  [line.BELGRAVE]: {
+    primary: "aus:vic:vic-02-BEG:",
+    replacementBus: ["aus:vic:vic-02-BEG-R:"],
+  },
 
-  // Lilydale
-  "aus:vic:vic-02-LIL:": { lineId: line.LILYDALE },
-  "aus:vic:vic-02-LIL-R:": { lineId: line.LILYDALE, replacementBus: true }, // Replacement bus
+  [line.LILYDALE]: {
+    primary: "aus:vic:vic-02-LIL:",
+    replacementBus: ["aus:vic:vic-02-LIL-R:"],
+  },
 
-  // Hurstbridge
-  "aus:vic:vic-02-HBE:": { lineId: line.HURSTBRIDGE },
-  "aus:vic:vic-02-HBE-R:": { lineId: line.HURSTBRIDGE, replacementBus: true }, // Replacement bus
+  [line.HURSTBRIDGE]: {
+    primary: "aus:vic:vic-02-HBE:",
+    replacementBus: ["aus:vic:vic-02-HBE-R:"],
+  },
 
-  // Mernda
-  "aus:vic:vic-02-MDD:": { lineId: line.MERNDA },
-  "aus:vic:vic-02-MDD-R:": { lineId: line.MERNDA, replacementBus: true }, // Replacement bus
+  [line.MERNDA]: {
+    primary: "aus:vic:vic-02-MDD:",
+    replacementBus: ["aus:vic:vic-02-MDD-R:"],
+  },
 
-  // Upfield
-  "aus:vic:vic-02-UFD:": { lineId: line.UPFIELD },
-  "aus:vic:vic-02-UFD-R:": { lineId: line.UPFIELD, replacementBus: true }, // Replacement bus
+  [line.UPFIELD]: {
+    primary: "aus:vic:vic-02-UFD:",
+    replacementBus: ["aus:vic:vic-02-UFD-R:"],
+  },
 
-  // Craigieburn
-  "aus:vic:vic-02-CGB:": { lineId: line.CRAIGIEBURN },
-  "aus:vic:vic-02-CGB-R:": { lineId: line.CRAIGIEBURN, replacementBus: true }, // Replacement bus
+  [line.CRAIGIEBURN]: {
+    primary: "aus:vic:vic-02-CGB:",
+    replacementBus: ["aus:vic:vic-02-CGB-R:"],
+  },
 
-  // Seymour
-  "aus:vic:vic-01-SER:": { lineId: line.SEYMOUR },
+  [line.SEYMOUR]: {
+    primary: "aus:vic:vic-01-SER:",
+    other: ["aus:vic:vic-01-ABY:", "aus:vic:vic-01-SNH:"],
+  },
 
-  // Flemington Racecourse
-  "aus:vic:vic-02-RCE:": { lineId: line.FLEMINGTON_RACECOURSE },
-  "aus:vic:vic-02-RCE-R:": { lineId: line.FLEMINGTON_RACECOURSE, replacementBus: true }, // Replacement bus
+  [line.FLEMINGTON_RACECOURSE]: {
+    primary: "aus:vic:vic-02-RCE:",
+    replacementBus: ["aus:vic:vic-02-RCE-R:"],
+  },
 
-  // Sunbury
-  "aus:vic:vic-02-SUY:": { lineId: line.SUNBURY },
-  "aus:vic:vic-02-SUY-R:": { lineId: line.SUNBURY, replacementBus: true }, // Replacement bus
+  [line.SUNBURY]: {
+    primary: "aus:vic:vic-02-SUY:",
+    replacementBus: ["aus:vic:vic-02-SUY-R:"],
+  },
 
-  // Bendigo
-  "aus:vic:vic-01-BGO:": { lineId: line.BENDIGO },
+  [line.BENDIGO]: {
+    primary: "aus:vic:vic-01-BGO:",
+    other: ["aus:vic:vic-01-ECH:", "aus:vic:vic-01-SWL:"],
+  },
 
-  // Ballarat
-  "aus:vic:vic-01-BAT:": { lineId: line.BALLARAT },
+  [line.BALLARAT]: {
+    primary: "aus:vic:vic-01-BAT:",
+    other: ["aus:vic:vic-01-ART:", "aus:vic:vic-01-MBY:"],
+  },
 
-  // Geelong
-  "aus:vic:vic-01-GEL:": { lineId: line.GEELONG },
+  [line.GEELONG]: {
+    primary: "aus:vic:vic-01-GEL:",
+    other: ["aus:vic:vic-01-WBL:"],
+  },
 
-  // Werribee
-  "aus:vic:vic-02-WER:": { lineId: line.WERRIBEE },
-  "aus:vic:vic-02-WER-R:": { lineId: line.WERRIBEE, replacementBus: true }, // Replacement bus
+  [line.WERRIBEE]: {
+    primary: "aus:vic:vic-02-WER:",
+    replacementBus: ["aus:vic:vic-02-WER-R:"],
+  },
 
-  // Williamstown
-  "aus:vic:vic-02-WIL:": { lineId: line.WILLIAMSTOWN },
-  "aus:vic:vic-02-WIL-R:": { lineId: line.WILLIAMSTOWN, replacementBus: true }, // Replacement bus
-
-  // Albury
-  "aus:vic:vic-01-ABY:": { lineId: line.ALBURY },
-
-  // Ararat
-  "aus:vic:vic-01-ART:": { lineId: line.ARARAT },
-
-  // Bairnsdale
-  "aus:vic:vic-01-BDE:": { lineId: line.BAIRNSDALE },
-
-  // Echuca
-  "aus:vic:vic-01-ECH:": { lineId: line.ECHUCA },
-
-  // Maryborough
-  "aus:vic:vic-01-MBY:": { lineId: line.MARYBOROUGH },
-
-  // Shepparton
-  "aus:vic:vic-01-SNH:": { lineId: line.SHEPPARTON },
-
-  // Swan Hill
-  "aus:vic:vic-01-SWL:": { lineId: line.SWAN_HILL },
-
-  // Warrnambool
-  "aus:vic:vic-01-WBL:": { lineId: line.WARRNAMBOOL },
+  [line.WILLIAMSTOWN]: {
+    primary: "aus:vic:vic-02-WIL:",
+    replacementBus: ["aus:vic:vic-02-WIL-R:"],
+  },
 };
--- src/third-party-id-mapping-types.ts
+++ src/third-party-id-mapping-types.ts
@@ -1,20 +1,21 @@
-export type LinePtvApiIdMapping = Record<string, number>;
-
 export type StopGtfsIds = {
   readonly parent: string;
-  readonly general?: string[];
+  readonly general?: readonly string[];
   readonly platforms?: Readonly<Record<number, readonly string[]>>;
-  readonly replacementBus?: string[];
+  readonly replacementBus?: readonly string[];
 };
 
-export type StopPtvIds = readonly string[];
+export type LineGtfsIds = {
+  readonly primary: string;
+  readonly other?: readonly string[];
+  readonly replacementBus?: readonly string[];
+};
+
+export type StopPtvApiIds = readonly string[];
+
+export type LinePtvApiIds = readonly string[];
 
 export type StopGtfsIdMapping = Record<number, StopGtfsIds>;
-export type StopPtvApiIdMapping = Record<number, StopPtvIds>;
-
-export type LineGtfsIdMapping = Record<string, LineGtfsIdMappingMetadata>;
-
-export type LineGtfsIdMappingMetadata = {
-  lineId: number;
-  replacementBus?: boolean;
-};
+export type LineGtfsIdMapping = Record<number, LineGtfsIds>;
+export type StopPtvApiIdMapping = Record<number, StopPtvApiIds>;
+export type LinePtvIdMapping = Record<number, LinePtvApiIds>;
