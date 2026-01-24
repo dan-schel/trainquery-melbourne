77d22176b42bd0b4b096c048eb0009388dddf700
dan-schel <me@danschellekens.com>
2026-01-24 21:20:15 +1100

    outline checks

--- scripts/compare-config-to-gtfs/main.ts
+++ scripts/compare-config-to-gtfs/main.ts
@@ -1,7 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/require-await
+import { readGtfs } from "../../src/gtfs/schedule/read-gtfs.js";
+import { withGtfsFiles } from "../../src/gtfs/schedule/with-gtfs-files.js";
+import { StopsCsvTree } from "../utils/gtfs/stops-csv-tree.js";
+import { env } from "./env.js";
+
 async function main() {
-  console.log("TODO: Implement");
-  console.log("✅ Done!");
+  console.log("Downloading/parsing GTFS data...");
+
+  const gtfsData = await withGtfsFiles(env.RELAY_KEY, readGtfs);
+  const allStops = StopsCsvTree.fromGtfsData(gtfsData);
+  console.log(`That's ${allStops.nodes.length} stops!`);
 }
 
 main().catch((error) => {
