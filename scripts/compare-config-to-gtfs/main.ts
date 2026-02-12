f9b81fca240fdb660197c9def4f45465ff5cc77e
dan-schel <me@danschellekens.com>
2026-02-12 20:45:54 +1100

    remove checks

--- scripts/compare-config-to-gtfs/main.ts
+++ scripts/compare-config-to-gtfs/main.ts
@@ -1,19 +1,12 @@
-import { checkCorridorsConsistent } from "./checks/check-corridors-consistent/index.js";
 import { env } from "./env.js";
-import { LintIssueReporter } from "./lint-issue-reporter.js";
 import { LintingContext } from "./linting-context.js";
 
 async function main() {
   console.log("Downloading/parsing GTFS data...");
 
   const ctx = await LintingContext.build(env.RELAY_KEY);
-  const reporter = new LintIssueReporter();
 
-  reporter.scope("Corridors consistent", () => {
-    checkCorridorsConsistent(ctx, reporter);
-  });
-
-  reporter.printSummary();
+  console.log(ctx.gtfsData.regional.trips.length);
 }
 
 main().catch((error) => {
