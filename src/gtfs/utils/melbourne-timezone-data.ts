d279f33f39c02f0b165ea86f8166e3ed644bdef3
dan-schel <me@danschellekens.com>
2026-08-03 20:09:40 +1000

    fix exports

--- src/gtfs/utils/melbourne-timezone-data.ts
+++ src/gtfs/utils/melbourne-timezone-data.ts
@@ -4,8 +4,8 @@ export const MELBOURNE_TIMEZONE = "Australia/Melbourne";
 
 // While we could cast a much wider net (+/- 24 hours) to handle all possible
 // timezones, we know that for Melbourne it's either +10 in AEST or +11 in AEDT.
-export const MELBOURNE_MINIMUM_VIABLE_OFFSET_SECONDS = 10 * 60 * 60;
-export const MELBOURNE_MAXIMUM_VIABLE_OFFSET_SECONDS = 11 * 60 * 60;
+const MELBOURNE_MINIMUM_VIABLE_OFFSET_SECONDS = 10 * 60 * 60;
+const MELBOURNE_MAXIMUM_VIABLE_OFFSET_SECONDS = 11 * 60 * 60;
 
 export const MELBOURNE_TIMEZONE_DATA: TimezoneData = {
   timezone: MELBOURNE_TIMEZONE,
