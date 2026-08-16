0dda540d2fd62efee0d981b1a30d42bd5b26bf2e
dan-schel <me@danschellekens.com>
2026-08-16 22:42:58 +1000

    update integration tests

--- tests/gtfs/corequery-gtfs/integration/2026-08-16-regional/index.test.ts
+++ tests/gtfs/corequery-gtfs/integration/2026-08-16-regional/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { createGtfsSystemForIntegrationTest } from "../support/create-gtfs-system-for-integration-test.js";
 
-describe("2026-08-16-suburban", () => {
+describe("2026-08-16-regional", () => {
   const system = createGtfsSystemForIntegrationTest(import.meta.dirname);
 
   // TODO: Or whatever.
