cfb5e0fe1e09bddcfe686a4afd5b025da16a6245
dan-schel <me@danschellekens.com>
2026-08-17 16:52:06 +1000

    delete corequery-gtfs code

--- src/gtfs/corequery-gtfs/data/utils.ts
+++
@@ -1,6 +0,0 @@
-import type { GtfsScheduledTripServicingMovement } from "./gtfs-scheduled-trip-movements.js";
-import type { GtfsUpdatedTripServicingMovement } from "./gtfs-updated-trip-movements.js";
-
-export type GtfsTripServicingMovement =
-  | GtfsScheduledTripServicingMovement
-  | GtfsUpdatedTripServicingMovement;
