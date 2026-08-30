4655992870024ae849809743a7fc84bfe0a3081a
Dan Schellekens <me@danschellekens.com>
2026-08-30 17:11:19 +1000

    Remove irrelevant TODO (#100)

--- knip.config.ts
+++ knip.config.ts
@@ -2,12 +2,6 @@ import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
   tags: ["-knipignore"],
-
-  // TODO: Remove this.
-  ignoreIssues: {
-    "src/gtfs/temp-script.ts": ["files"],
-    "src/gtfs/retrieval/realtime/fetch-gtfs-realtime.ts": ["exports"],
-  },
 };
 
 export default config;
